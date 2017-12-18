bc218cba17962fdda5fec6b4226eb2aededd5a0a
ogaillard63 <ogaillard63@gmail.com>
2017-12-18 10:26:22 +0100

    clean

--- inc/prepend.php
+++ inc/prepend.php
@@ -21,9 +21,9 @@ setlocale(LC_ALL, 'fr_FR');
 date_default_timezone_set('Europe/Paris');
 
 define('DS', '/');
-define('PATH_APP', 			realpath(dirname (__FILE__).'/..'));
-define('PATH_INC', 			PATH_APP.'/inc');
-define('PATH_LANG',			PATH_APP.'/lang');
+define('BASE_DIR', 			realpath(dirname (__FILE__).'/..'));
+define('PATH_INC', 			BASE_DIR.'/inc');
+define('PATH_LANG',			BASE_DIR.'/lang');
 define('PATH_PROPERTIES', 	PATH_INC.'/properties');
 define('PATH_CLASSES', 		PATH_INC.'/classes');
 
@@ -32,9 +32,13 @@ $properties_filepath = PATH_PROPERTIES.DS. $_SERVER["HTTP_HOST"].DS.'properties.
 if (!is_file($properties_filepath)) die("Unbale to find :  ".$properties_filepath);
 $prop = parse_ini_file($properties_filepath);
 
+// Chemin de la webapp
+$path_app = $prop['path_app'];
+$smarty->assign('path_app', $path_app);
+
 // Paramètres des chemins d'accès
 define('PATH_TPL_RELATIVE',		'tpl'); 
-define('PATH_TPL', 				PATH_APP.DS.PATH_TPL_RELATIVE.DS); // Dossier des templates de vues
+define('PATH_TPL', 				BASE_DIR.DS.PATH_TPL_RELATIVE.DS); // Dossier des templates de vues
 
 // Session
 $session = Session::getInstance();
@@ -47,10 +51,7 @@ $smarty->compile_dir  = 'tpl_cache';
 $smarty->compile_check = true;
 $smarty->force_compile = true;
 $smarty->assign('tpl', PATH_TPL_RELATIVE);
-$array = explode(DIRECTORY_SEPARATOR, dirname (__FILE__));
-$path_app = "/".$array[count($array)-2];
-$smarty->assign('path_app', $path_app);
-//$smarty->assign('path_app', "");
+
 
 
 // Connexion à la base de données
@@ -84,7 +85,7 @@ if ($alert = $session->getValue("alert")) {
 }
 
 // Génération du menu dynamique
-foreach (glob(PATH_APP. "/*.php") as $filename) {
+foreach (glob(BASE_DIR. "/*.php") as $filename) {
 	$fi = pathinfo($filename);
 	if ($fi['filename'] != "index") {
 		$smarty->append("choices", array("link" => $fi['basename'],"title" => $fi['filename']));
